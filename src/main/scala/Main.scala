e0c5ca053d88abea80b02a414eec17affebcc963
Benyamain Yacoob <yacoobbenyamain@gmail.com>
2023-09-23 21:23:03 -0400

    warmed up a bit with scala, now let's build something cool:sunglasses::100::thumbsup::smirk:

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,13 @@
 package org.ben.beginner
 
 object Main extends App {
+  val robots = List(
+    Robot(firstName = "Dolph", lastName = "The Robot", 553),
+    Robot(firstName = "Sylvester", lastName = "The Robot", 554),
+    Robot(firstName = "Arnold", lastName = "The Terminator", 555),
+  )
 
+  val filterRobot = Robot.filterRobot(robots)
+  private val descriptions = filterRobot.map(p => p.description).mkString("\n\t")
+  println(s"The robots are \n\t$descriptions")
 }
